e56d1bfdf26b93036cb9204dd4cecc66ef499f0c
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2024-11-18 12:36:03 +0600

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "./firebase.config";
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const response = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = response.user;
+    await updateProfile(user, { displayName: name });
+    await sendEmailVerification(user);
+    await signOut(auth);
+  } catch (error) {
+    console.error("Error registering user: ", error.message);
+  }
+};
+
+export { registerWithEmailAndPassword };
